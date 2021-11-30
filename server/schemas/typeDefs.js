b09f4980ea2c6b94d8c5aab7ab8ce6e576feee3e
jessicamcg <jessicamcguico@gmail.com>
2021-11-30 14:36:02 -0600

    server/schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,39 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedMovies: [Movies]
+    }
+
+    type Movies {
+        _id: ID
+        title: String
+        rating: Int
+        profit: Int
+        year: Int
+    }
+    
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        users: [User]
+        user(username: String!): User
+        savedMovies(username: String): [Movies]
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+    }
+`
+
+
+module.exports = typeDefs;
