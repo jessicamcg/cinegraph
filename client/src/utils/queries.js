1206fd5acc3aa2b49170980ad9dcc22118148c5a
jessicamcg <jessicamcguico@gmail.com>
2021-12-01 10:58:19 -0600

    queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,47 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      movies {
+        _id
+        title
+        rating
+        profit
+        year
+      }
+    }
+  }
+`;
+
+export const QUERY_MOVIES = gql`
+  query getMovies {
+      movies {
+        _id
+        title
+        rating
+        profit
+        year
+      }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      movies {
+        _id
+        title
+        rating
+        profit
+        year
+      }
+    }
+  }
+`;
