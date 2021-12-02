7c65485892f4cc3e87bf78d4575ccb736d8eceff
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-12-02 10:16:33 -0600

    test

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,57 +4,30 @@ const { Movies, User } = require('../models')
 const { movieData } = require('../utils/movieQuery')
 
 const resolvers = {
-  Query: {
-    users: async () => {
-      return User.find().populate('movies');
-    },
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('movies');
-    },
-    me: async (parent, args, context) => {
-      if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate('movies');
+    Query: {
+      users: async () => {
+        return User.find().populate('movies');
+      },
+      user: async (parent, { username }) => {
+        return User.findOne({ username }).populate('movies');
+      },
+      me: async (parent, args, context) => {
+        if (context.user) {
+          return User.findOne({ _id: context.user._id }).populate('movies');
+        }
+        throw new AuthenticationError('You need to be logged in!');
+      },
+      
+       movieData: async ()=>{
+         const data = await movieData.movieQuery();
+         const movieInfo = data.data
+         movieInfo.Rating = data.data.Ratings[1].Value
+         console.log(movieInfo)
+         return movieInfo
+        
+       
       }
-      throw new AuthenticationError('You need to be logged in!');
     },
-<<<<<<< HEAD
-    Mutation: {
-        saveOmdbMovie: async(parent, args) =>{
-          const data = await movieData.movieQuery();
-          const movieInfo = {}
-          movieInfo.title = data.data.Title
-          movieInfo.rating = data.data.Ratings[1].Value
-          movieInfo.profit = data.data.BoxOffice
-          movieInfo.year = data.data.Year
-          console.log(movieInfo)
-          return Movies.create(movieInfo)
-          // movieInfo.title = data.data.Title
-          // movieInfo.title = data.data.Title
- 
-        },
-    //     addUser: async (parent, { username, email, password }) => {
-    //         const user = await User.create({ username, email, password });
-    //         const token = signToken(user);
-    //         return { token, user };
-    //     },
-    //     login: async (parent, { email, password }) => {
-    //       const user = await User.findOne({ email });
-    
-    //       if (!user) {
-    //         throw new AuthenticationError('No user found with this email address');
-    //       }
-      
-    //       const correctPw = await user.isCorrectPassword(password);
-      
-    //       if (!correctPw) {
-    //          throw new AuthenticationError('Incorrect credentials');
-    //       }
-      
-    //       const token = signToken(user);
-      
-    //       return { token, user };
-    //     },
-=======
 
     movieData: async () => {
       const data = await movieData.movieQuery();
@@ -64,8 +37,8 @@ const resolvers = {
       return movieInfo
 
 
-    }
-  },
+    },
+  
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
       const user = await User.create({ username, email, password });
@@ -103,26 +76,25 @@ const resolvers = {
 
     },
 
->>>>>>> main
     //     saveMovie: async () => {
     //       const savedMovie = await Movie.create(args);
     //         return savedMovie;
     //     },
-    removeMovie: async (parent, { imdbID }, context) => {
-      if (context.user) {
-        return Movie.findOneAndUpdate(
-          { _id: imdbID },
-          {
-            $pull: {
-              movies: { _id: imdbID },
-            },
-          },
-          { new: true }
-        );
-      }
+    //     removeMovie: async (parent, { imdbID }, context) => {
+    //       if (context.user) {
+    //           return Movie.findOneAndUpdate(
+    //             { _id: imdbID },
+    //             {
+    //               $pull: {
+    //                 movies: { _id: imdbID },
+    //               },
+    //             },
+    //             { new: true }
+    //           );
+    //       }
+    //     }
     }
   }
-}
 
 
 module.exports = resolvers;
