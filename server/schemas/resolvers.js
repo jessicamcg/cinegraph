eff2820e2ca8e659053dcef6d283aecf21c46128
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-12-02 00:32:16 -0600

    working on Movies.create

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
-const { Movie, User } = require('../models')
+const { Movies, User } = require('../models')
 const { movieData } = require('../utils/movieQuery')
 
 const resolvers = {
@@ -32,12 +32,12 @@ const resolvers = {
         saveOmdbMovie: async(parent, args) =>{
           const data = await movieData.movieQuery();
           const movieInfo = {}
-          movieInfo.Title = data.data.Title
-          movieInfo.Rating = data.data.Ratings[1].Value
-          movieInfo.BoxOffice = data.data.BoxOffice
-          movieInfo.Year = data.data.Year
+          movieInfo.title = data.data.Title
+          movieInfo.rating = data.data.Ratings[1].Value
+          movieInfo.profit = data.data.BoxOffice
+          movieInfo.year = data.data.Year
           console.log(movieInfo)
-          return movieInfo
+          return Movies.create(movieInfo)
           // movieInfo.title = data.data.Title
           // movieInfo.title = data.data.Title
  
