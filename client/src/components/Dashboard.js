02b919be6ae13145520ec498137f71b21ba64f01
jessicamcg <jessicamcguico@gmail.com>
2021-12-01 23:44:46 -0600

    dashboard skeleton

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Typography from '@mui/material/Typography';
+
+export default function Dashboard() {
+    return (
+        <Box>
+            <Typography variant='h3'>
+                dashboard
+            </Typography>
+        </Box>
+    )
+}
