654d2ebe87687d7d0f200d19dd3e2580da858c4b
Juri <jasonluxie@gmail.com>
2021-12-03 11:20:14 -0600

    home page more defined, needs styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,54 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 
-
-
+// hero cinegraph
+// description
+// call to action
 
 const Home = () => {
     return (
         <Box>
-            <Typography variant='h3'>
-                home page
-            </Typography>
-            <Typography variant='body1' gutterbottom>
-                and explaination of application
-            </Typography>
-            <Typography variant='body1' gutterbottom>
-                you should log in (or sign up) to access full features
-            </Typography>
+            <Box className="jumbotron">
+                <Typography variant="h1" align="center">Cinegraph</Typography>
+            </Box>
+            <Container className="description">
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        <Typography variant="h5" align="center">
+                            explaination
+                        </Typography>
+                        <Typography variant="body1" align="center">
+                            explaination details
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Typography variant="h5" align="center">
+                            of
+                        </Typography>
+                        <Typography variant="body1" align="center">
+                            more details
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Typography variant="h5" align="center">
+                            application
+                        </Typography>
+                        <Typography variant="body1" align="center">
+                            even more details
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Container>
+            <Container className="call-to-action">
+                <Typography variant="body1" gutterbottom>
+                    you should log in (or sign up) to access full features
+                </Typography>
+            </Container>
         </Box>
-    )
+    );
 };
-
 
 export default Home;
