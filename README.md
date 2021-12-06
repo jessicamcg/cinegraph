9ede299c2cc46d68d973c8c87491ec29fccd8305
Juri <jasonluxie@gmail.com>
2021-12-06 13:50:21 -0600

    some readme

--- README.md
+++ README.md
@@ -1,13 +1,35 @@
 # project-3
 
+## Description
+
+Cinegraph is
+
+## Deployment
+
+The website is deployed on [Heroku](https://movie-data-model.herokuapp.com/)
 
 ## Contributors
-[Jessica Guico](https://github.com/jessicamcg)
 
-[Nick Margaritondo](https://github.com/Nickm615)
+-   [Jessica Guico](https://github.com/jessicamcg)
 
-[Jason Xie](https://github.com/jasonluxie)
+-   [Nick Margaritondo](https://github.com/Nickm615)
 
+-   [Jason Xie](https://github.com/jasonluxie)
 
-## Live Link
-[Heroku](https://movie-data-model.herokuapp.com/)
+## Demonstration
+
+## Resources
+
+### Framework & Components
+
+-   [MUI](https://mui.com/)
+
+## Further Development
+
+### Dashboard
+
+-   Display movie poster when querying for movies
+
+### Signup
+
+-   OAuth
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,47 +14,81 @@ const Home = () => {
     return (
         <Box>
             <Box className="jumbotron">
-                <Typography variant="h1" align="center">Cinegraph</Typography>
+                <Typography variant="h1" align="center">
+                    Cinegraph
+                </Typography>
             </Box>
             <Container className="description">
                 <Grid container spacing={2}>
                     <Grid item xs={4}>
-                        <Typography variant="h5" align="center" className='description-header'>
+                        <Typography
+                            variant="h5"
+                            align="center"
+                            className="description-header"
+                        >
                             Visualize Cinema Data
                         </Typography>
-                        <Typography variant="body1" align="center" className='description-content'>
-                            Satisfy your curiosity by searching and saving relational film data which you can quickly see plotted in comparison to other films. See how box office and budget trend together. 
+                        <Typography
+                            variant="body1"
+                            align="center"
+                            className="description-content"
+                        >
+                            Satisfy your curiosity by searching and saving
+                            relational film data which you can quickly see
+                            plotted in comparison to other films. See how box
+                            office and budget trend together.
                         </Typography>
                     </Grid>
                     <Grid item xs={4}>
-                        <Typography variant="h5" align="center"className='description-header'>
+                        <Typography
+                            variant="h5"
+                            align="center"
+                            className="description-header"
+                        >
                             More details
                         </Typography>
-                        <Typography variant="body1" align="center" className='description-content'>
+                        <Typography
+                            variant="body1"
+                            align="center"
+                            className="description-content"
+                        >
                             more details
                         </Typography>
                     </Grid>
                     <Grid item xs={4}>
-                        <Typography variant="h5" align="center"className='description-header'>
+                        <Typography
+                            variant="h5"
+                            align="center"
+                            className="description-header"
+                        >
                             Adaptation Potential
                         </Typography>
-                        <Typography variant="body1" align="center" className='description-content'>
-                            Cinegraph is a proof of concept showing that we could work with large datasets and visually display them. With minor adjustments, this application can be applied to work with any large dataset as a visaulization and analysis tool. 
+                        <Typography
+                            variant="body1"
+                            align="center"
+                            className="description-content"
+                        >
+                            Cinegraph is a proof of concept showing that we
+                            could work with large datasets and visually display
+                            them. With minor adjustments, this application can
+                            be applied to work with any large dataset as a
+                            visaulization and analysis tool.
                         </Typography>
                     </Grid>
                 </Grid>
             </Container>
             <Container className="call-to-action">
-                {!Auth.loggedIn()
-                    ? <Typography variant="h4" align="center">
-                        Please log in or sign up to access our full functionality.
-                      </Typography>
-                    : <Typography variant="h4" align="center">
+                {!Auth.loggedIn() ? (
+                    <Typography variant="h4" align="center">
+                        Please log in or sign up to access our full
+                        functionality.
+                    </Typography>
+                ) : (
+                    <Typography variant="h4" align="center">
                         You are logged in! Use the navigation links above to
                         view your dashboard!
-                      </Typography>
-                }
-                
+                    </Typography>
+                )}
             </Container>
         </Box>
     );
