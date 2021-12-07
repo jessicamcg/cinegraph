3695a493015d41f79d64933407df4eeb71e3d9c1
Juri <jasonluxie@gmail.com>
2021-12-06 21:51:29 -0600

    login/signup styling

--- client/src/pages/LoginForm.js
+++ client/src/pages/LoginForm.js
@@ -4,11 +4,28 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
+import Container from "@mui/material/Container";
+import { ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { useMutation } from "@apollo/client";
 import { LOGIN } from "../utils/mutations";
 import Auth from "../utils/auth";
+import "../styles/login-signup.css";
 
 export default function LoginForm() {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: "#1b5e20",
+            },
+            secondary: {
+                main: "#77d4d8",
+            },
+            info: {
+                main: "#ffffff",
+            },
+        },
+    });
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [login, { error }] = useMutation(LOGIN);
@@ -27,49 +44,58 @@ export default function LoginForm() {
     };
 
     return (
-        <Box
-            component="form"
-            sx={{
-                "& .MuiTextField-root": { m: 1, width: "25ch" },
-                display: "flex",
-                alignItems: "flex-start",
-                flexDirection: "column",
-            }}
-            noValidate
-            autoComplete="off"
-            onSubmit={handleSubmit}
-        >
-            <Typography variant="h3" component="div" gutterBottom>
-                Log In
-            </Typography>
-            <TextField
-                required
-                id="outlined-required"
-                label="Email"
-                value={email}
-                onInput={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-                required
-                id="outlined-password-input"
-                label="Password"
-                type="password"
-                autoComplete="current-password"
-                value={password}
-                onInput={(e) => setPassword(e.target.value)}
-            />
+        <ThemeProvider theme={theme}>
+            <Container className="form-holder">
+                <Box
+                    component="form"
+                    sx={{
+                        "& .MuiTextField-root": { m: 1, width: "25ch" },
+                        display: "flex",
+                        alignItems: "center",
+                        flexDirection: "column",
+                    }}
+                    noValidate
+                    autoComplete="off"
+                    onSubmit={handleSubmit}
+                >
+                    <Typography variant="h3" component="div">
+                        Log In
+                    </Typography>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Email"
+                        value={email}
+                        onInput={(e) => setEmail(e.target.value)}
+                    />
+                    <TextField
+                        required
+                        id="outlined-password-input"
+                        label="Password"
+                        type="password"
+                        autoComplete="current-password"
+                        value={password}
+                        onInput={(e) => setPassword(e.target.value)}
+                    />
 
-            {error ? (
-                <Typography variant="body1" gutterBottom>
-                    Incorrect credentials
-                </Typography>
-            ) : null}
-            <Button type="submit" variant="contained">
-                Submit
-            </Button>
-            <Typography variant="caption" gutterBottom>
-                Don't have an account?<Link href="signup">Signup</Link>instead.
-            </Typography>
-        </Box>
+                    {error ? (
+                        <Typography variant="body1" gutterBottom>
+                            Incorrect credentials
+                        </Typography>
+                    ) : null}
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="secondary"
+                        className="button"
+                    >
+                        Submit
+                    </Button>
+                    <Typography variant="caption" gutterBottom class="redirect-text">
+                        Don't have an account? <Link href="signup">Signup</Link> instead.
+                    </Typography>
+                </Box>
+            </Container>
+        </ThemeProvider>
     );
 }
