d73f74eb95053f3fdb75892835a67a8142ae175c
Juri <jasonluxie@gmail.com>
2021-12-06 18:40:04 -0600

    added theming to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,38 +3,56 @@ import Auth from "../utils/auth";
 import Button from "@mui/material/Button";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar"
+import { ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 
 function Navbar() {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: "#4caf50"
+            },
+            secondary: {
+                main: "#77d4d8",
+            },
+            info: {
+                main: "#000000"
+            }
+        }
+    })
+
     function showNavigation() {
         return Auth.loggedIn() ? (
             <>
-                <Button href="dashboard" color="warning">
+                <Button href="dashboard" color="info">
                     Dashboard
                 </Button>
                 <Button
                     href="/"
-                    color="warning"
+                    color="info"
                     onClick={() => Auth.logout()}
                 >
                     Log Out
                 </Button>
             </>
         ) : (
-            <Button href="login" color="warning">
+            <Button href="login" color="info">
                 Log In
             </Button>
         );
     }
 
     return (
-        <AppBar className="navbar" position="fixed">
+        <ThemeProvider theme={theme}>
+        <AppBar className="navbar" position="fixed" color="primary">
             <Toolbar variant="dense">
-            <Button href="/" color="warning">
+            <Button href="/" color="info">
                 Home
             </Button>
             {showNavigation()}
             </Toolbar>
         </AppBar>
+        </ThemeProvider>
     );
 }
 export default Navbar;
